00424370fbf5c7d4ab9f862b01bd2c7f964745da
Jenifer Machado <yenifermg1798@gmail.com>
2026-08-24 15:29:18 +0200

    chore: restore previous database changes

---
+++ DataStructureDoc.md
@@ -0,0 +1,67 @@
+# Estructure od data dependencies
+
+This doc defined de structure, propefrties, uses, dependencies and relationships of the four bases data structure of this app in order to create a smooth migration to a data base system.
+
+## Groupos
+
+Defined all world cup groups one by one
+
+### Structure
+[
+    {
+        "id": 1,
+        "name": "Grupo A",
+        "code": "A"
+    }
+]
+
+### Properties
+id(number) - Unique group identifier.
+name(string) - Group name (`Grupo ${code}`).
+code(string) - Group letter.
+
+### Where used
+
+
+### Dependencies
+
+### Relationships
+
+---
+
+## Matches
+### Structure
+
+### Properties
+
+### Where used
+
+### Dependencies
+
+### Relationships
+
+---
+
+## MatchTeams
+### Structure
+
+### Properties
+
+### Where used
+
+### Dependencies
+
+### Relationships
+
+---
+
+## Teams
+### Structure
+
+### Properties
+
+### Where used
+
+### Dependencies
+
+### Relationships
--- src/data/groups.js
+++ src/data/groups.js
@@ -8,65 +8,65 @@
  */
 const groups = [
   {
-    "id": 1,
-    "name": "Grupo A",
-    "code": "A"
+    id: 1,
+    name: "Grupo A",
+    code: "A",
   },
   {
-    "id": 2,
-    "name": "Grupo B",
-    "code": "B"
+    id: 2,
+    name: "Grupo B",
+    code: "B",
   },
   {
-    "id": 3,
-    "name": "Grupo C",
-    "code": "C"
+    id: 3,
+    name: "Grupo C",
+    code: "C",
   },
   {
-    "id": 4,
-    "name": "Grupo D",
-    "code": "D"
+    id: 4,
+    name: "Grupo D",
+    code: "D",
   },
   {
-    "id": 5,
-    "name": "Grupo E",
-    "code": "E"
+    id: 5,
+    name: "Grupo E",
+    code: "E",
   },
   {
-    "id": 6,
-    "name": "Grupo F",
-    "code": "F"
+    id: 6,
+    name: "Grupo F",
+    code: "F",
   },
   {
-    "id": 7,
-    "name": "Grupo G",
-    "code": "G"
+    id: 7,
+    name: "Grupo G",
+    code: "G",
   },
   {
-    "id": 8,
-    "name": "Grupo H",
-    "code": "H"
+    id: 8,
+    name: "Grupo H",
+    code: "H",
   },
   {
-    "id": 9,
-    "name": "Grupo I",
-    "code": "I"
+    id: 9,
+    name: "Grupo I",
+    code: "I",
   },
   {
-    "id": 10,
-    "name": "Grupo J",
-    "code": "J"
+    id: 10,
+    name: "Grupo J",
+    code: "J",
   },
   {
-    "id": 11,
-    "name": "Grupo K",
-    "code": "K"
+    id: 11,
+    name: "Grupo K",
+    code: "K",
   },
   {
-    "id": 12,
-    "name": "Grupo L",
-    "code": "L"
-  }
+    id: 12,
+    name: "Grupo L",
+    code: "L",
+  },
 ];
 
 export default groups;
